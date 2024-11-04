d505cd37363a6bbf41b79ecb0d83a3a535a1d834
npaltiel <nochum.paltiel@gmail.com>
2024-11-04 16:54:53 -0500

    Finished report now with 3 categories for each Team and Contract Type (Prior to Sep 1, In 5 Boroughs and Outside 5 Boroughs)

--- team_hours.py
+++ team_hours.py
@@ -4,6 +4,9 @@ import sqlite3
 from datetime import date, datetime, timedelta
 from dateutil.relativedelta import relativedelta
 
+month = datetime.today() - relativedelta(months=1)
+month = 'July'
+
 
 def convert_hours(value):
     if isinstance(value, str) and value.strip() == '':
@@ -23,89 +26,45 @@ converters = {col: convert_hours for col in hour_columns}
 df_patients = pd.read_csv(
     "C:\\Users\\nochum.paltiel\\OneDrive - Anchor Home Health care\\Documents\\General Information\\List of Patients.csv")
 invoiced_visits = pd.read_excel(
-    "C:\\Users\\nochum.paltiel\\OneDrive - Anchor Home Health care\\Documents\\Team Hours\\Billing_Vs_Payroll_1019_1025.xlsx",
+    f"C:\\Users\\nochum.paltiel\\OneDrive - Anchor Home Health care\\Documents\\Team Hours\\Billing_Vs_Payroll_{month}.xlsx",
     sheet_name='Detail Data', converters=converters)
 
 invoiced_visits = invoiced_visits[invoiced_visits['Contract'] != 'Grand Total :']
 invoiced_visits['Billed Hours'] = invoiced_visits['Billed Hours'] + 13 * invoiced_visits['Billed Live-In']
 
-invoiced_visits['Invoice Date'] = pd.to_datetime(invoiced_visits['Invoice Date'], errors='coerce')
-temp_date = max(invoiced_visits['Invoice Date'])
-week_ending = temp_date + timedelta(days=4 - temp_date.weekday())
+# Get Patient Data
+# Create UniqueID for Patients
+df_patients['UniqueID'] = [
+    df_patients['Medicaid Number'][i] if pd.notna(df_patients['Medicaid Number'][i]) and df_patients['Medicaid Number'][
+        i] != 0 else
+    df_patients['Patient Name'][i] + str(df_patients['DOB'][i]) for i in range(len(df_patients))]
+# Get original Patient Start Date
+df_patients['Patient Start Date'] = pd.to_datetime(df_patients['Patient Start Date'])
 
-invoiced_visits['Patient Start Date'] = pd.to_datetime(invoiced_visits['Patient Start Date'])
-
-earliest_start_date = invoiced_visits.groupby(['Admission ID']).agg({
-    'Billed Hours': 'sum',  # Sum of Billed Hours for each Medicaid Number
+earliest_start_date = df_patients.groupby(['UniqueID']).agg({
     'Patient Start Date': 'min'  # Earliest Patient Start Date for each Medicaid Number
 }).reset_index()
 
-# Step 2: Get Team and County based on the latest Patient Start Date
-latest_info = invoiced_visits.sort_values(by=['Medicaid Number', 'Patient Start Date'], ascending=[True, False])
-latest_info = latest_info.groupby(['Medicaid Number']).agg({
-    'Team': 'first',  # Team corresponding to the latest Patient Start Date
-    'County': 'first'  # County corresponding to the latest Patient Start Date
+# Get Medicaid, Team and County
+df_patients = df_patients[
+    ['Admission ID - Office', 'Contract Name', 'UniqueID', 'Team', 'County']]
+df_patients = df_patients.groupby(['Admission ID - Office', 'Contract Name'], as_index=False).first()
+invoiced_visits = pd.merge(invoiced_visits, df_patients, left_on=['Admission ID', 'Contract'],
+                           right_on=['Admission ID - Office', 'Contract Name'],
+                           how='left')
+invoiced_visits = pd.merge(invoiced_visits, earliest_start_date, on='UniqueID',
+                           how='left')
+
+team_billed_hours = invoiced_visits.groupby(['Admission ID']).agg({
+    'Contract': lambda x: x.mode().iloc[0],
+    'UniqueID': 'first',
+    'Billed Hours': 'sum',  # Sum of Billed Hours for each Medicaid Number
+    'Patient Start Date': 'first',
+    'Team': 'first',
+    'County': 'first'
 }).reset_index()
 
-# Step 3: Merge the two results
-team_billed_hours = pd.merge(earliest_start_date, latest_info, on='Medicaid Number', suffixes=('', '_x'))
-team_billed_hours.insert(0, 'Week Ending', week_ending)
-
-# # Fix IC
-# ic_medicaids = {}
-# for i in range(len(invoiced_visits)):
-#     team = invoiced_visits['Team'][i]
-#     contract = invoiced_visits['Contract'][i]
-#     if team is not None and isinstance(team, str) and 'IC' in team and contract in ['TBI', 'NHTD']:
-#         ic_medicaids[invoiced_visits['Medicaid Number'][i]] = team
-#
-# admission_ids = [id for id in ic_medicaids]
-#
-# conn = sqlite3.connect(
-#     "C:\\Users\\nochum.paltiel\\OneDrive - Anchor Home Health care\\Documents\\General Information\\Visits\\Visits.db")
-#
-# query = "SELECT * FROM visits WHERE MedicaidNo IN ({seq})".format(
-#     seq=','.join(['?'] * len(admission_ids))
-# )
-#
-# visits_df = pd.read_sql_query(query, conn, params=admission_ids)
-#
-# conn.close
-#
-# visits_df = visits_df[(visits_df['MissedVisit'] == 'No') & (visits_df['Billed'] == 'Yes') & ~(visits_df['ContractName'].isin(['TBI', 'NHTD']))]
-#
-#
-# def calculate_scheduled_hours(schedule_time):
-#     # Split the ScheduleTime to get start and end times
-#     start_str, end_str = schedule_time.split('-')
-#
-#     # Parse start and end times to datetime objects
-#     start_time = datetime.strptime(start_str, '%H%M')
-#     end_time = datetime.strptime(end_str, '%H%M')
-#
-#     # Calculate the difference, accounting for crossing midnight
-#     if end_time <= start_time:
-#         end_time += timedelta(days=1)  # Adjust end time to next day if it crosses midnight
-#
-#     # Calculate total hours as a float
-#     scheduled_hours = (end_time - start_time).total_seconds() / 3600
-#     return scheduled_hours
-#
-#
-# # Apply the function to calculate ScheduledHours
-# visits_df['MaxHours'] = visits_df['ScheduleTime'].apply(calculate_scheduled_hours)
-#
-# visits_df['Year'] = pd.DatetimeIndex(visits_df['VisitDate']).year
-# visits_df['Month'] = pd.DatetimeIndex(visits_df['VisitDate']).month
-#
-# # Group by AdmissionId, Year, and Month and sum ScheduledHours
-# monthly_hours = visits_df.groupby(['MedicaidNo', 'Year', 'Month'])['MaxHours'].sum().reset_index()
-# max_hours = monthly_hours.groupby('MedicaidNo')['MaxHours'].max().reset_index()
-# max_hours['MaxHours'] = max_hours['MaxHours'] / 4.345
-#
-# team_billed_hours = pd.merge(team_billed_hours, max_hours, left_on='Medicaid Number', right_on='MedicaidNo',
-#                              how='left')
-
+# Fix IC
 team_billed_hours['Hours'] = [
     team_billed_hours['Billed Hours'][i] / 2 if team_billed_hours['Team'][i] is not None and isinstance(
         team_billed_hours['Team'][i], str) and 'IC' in team_billed_hours['Team'][i] and team_billed_hours['Contract'][
@@ -113,5 +72,45 @@ team_billed_hours['Hours'] = [
                                                     'TBI', 'NHTD'] else team_billed_hours['Billed Hours'][i] for i in
     range(len(team_billed_hours))]
 
-team_hours_report = team_billed_hours.groupby(['Team', 'Medicaid Number', 'Contract', 'County', 'Patient Start Date'])[
-    'Total Hours'].sum().reset_index()
+# Fix Non Billable Contracts
+temp_patients = df_patients[~(df_patients['Contract Name'] == 'Non Billable')]
+contract_lookup = temp_patients.set_index('Admission ID - Office')['Contract Name'].to_dict()
+
+team_billed_hours['Contract'] = team_billed_hours.apply(
+    lambda row: row['Contract'] if row['Contract'] != 'Non Billable'
+    else contract_lookup.get(row['Admission ID'], row['Contract']),
+    axis=1
+)
+
+# Get Contract Type
+df_contracts = pd.read_csv(
+    "C:\\Users\\nochum.paltiel\\OneDrive - Anchor Home Health care\\Documents\\General Information\\Contract Lookup.csv")
+team_billed_hours = pd.merge(team_billed_hours, df_contracts, left_on='Contract', right_on='ContractName', how='left')
+team_billed_hours['Contract Type'] = team_billed_hours['ContractType'].fillna('Unknown')
+
+boroughs = ['new york', 'kings', 'queens', 'bronx', 'richmond']
+grouping = []
+for i in range(len(team_billed_hours)):
+    if team_billed_hours['Patient Start Date'][i] < pd.to_datetime('2024-09-01'):
+        grouping.append('Prior to Sep 1')
+    else:
+        if team_billed_hours['County'][i] is not None and team_billed_hours['County'][i].lower() in boroughs:
+            grouping.append('In 5 Boroughs')
+        else:
+            grouping.append('Outside 5 Boroughs')
+team_billed_hours['Group'] = grouping
+
+# Final Report
+team_hours_report = \
+    team_billed_hours.groupby(['Team', 'Contract Type', 'Group'])[
+        'Hours'].sum().reset_index()
+
+# invoiced_visits['Invoice Date'] = pd.to_datetime(invoiced_visits['Invoice Date'], errors='coerce')
+# temp_date = max(invoiced_visits['Invoice Date'])
+# week_ending = (temp_date + timedelta(days=4 - temp_date.weekday())).strftime('%m-%d-%Y')
+#
+# team_hours_report.insert(0, 'Week Ending', week_ending)
+# team_hours_report['Patient Start Date'] = team_hours_report['Patient Start Date'].dt.strftime('%m/%d/%Y')
+
+excel_file = f"C:\\Users\\nochum.paltiel\\OneDrive - Anchor Home Health care\\Documents\\Team Hours\\Team_Hours_Report_{month}.xlsx"
+team_hours_report.to_excel(excel_file, index=False, sheet_name='Sheet1')
